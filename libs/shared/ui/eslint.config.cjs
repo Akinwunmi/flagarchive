5d8963e0930d48b9c183ad56632d9bc53ce1e4a4
Jurrit van der Ploeg <jurrit.vander.ploeg@ndwcloud.nu>
2025-01-15 16:11:28 +0100

    chore: publishable ui lib

--- libs/shared/ui/eslint.config.cjs
+++ libs/shared/ui/eslint.config.cjs
@@ -3,6 +3,20 @@ const baseConfig = require('../../../eslint.config.cjs');
 
 module.exports = [
   ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
+        },
+      ],
+    },
+    languageOptions: {
+      parser: require('jsonc-eslint-parser'),
+    },
+  },
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   {
