3a8fe49d3022fc56561a89ecba3446272afd0cff
Jurrit van der Ploeg <jurritvanderploeg.dev@gmail.com>
2025-06-17 09:27:17 +0200

    feat(breadcrumbs): show labels / convert crud to resources

--- apps/flagarchive/src/app/services/supabase.service.ts
+++ apps/flagarchive/src/app/services/supabase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { createClient, PostgrestError } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 import { environment } from '../../environments/environment';
 import { Database } from '../models';
@@ -16,12 +16,5 @@ export class SupabaseService {
 
   get flagOfTheDay() {
     return this.supabase.from('flag_of_the_day');
-  }
-
-  handleError<T>(response: { data: T | null; error: PostgrestError | null }): T {
-    if (response.error) {
-      throw new Error(response.error.message);
-    }
-    return response.data as T;
   }
 }
