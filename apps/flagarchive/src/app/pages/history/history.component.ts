a1bce2dfa49f0956dd4882aa3b94f306f42c8bf2
Jurrit van der Ploeg <jurritvanderploeg.dev@gmail.com>
2025-06-25 22:39:05 +0200

    bug(history): fix showing all flags and categories

--- apps/flagarchive/src/app/pages/history/history.component.ts
+++ apps/flagarchive/src/app/pages/history/history.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, computed, inject, linkedSignal } from '@angular/core';
 import { FlagCategory } from '@flagarchive/advanced-search';
-import { EntityFlagRange, EntityRange, Flag } from '@flagarchive/entities';
+import { EntityFlag, EntityFlagRange, EntityRange, Flag } from '@flagarchive/entities';
 import { HyphenatePipe, TagComponent, TagGroupComponent } from '@flagarchive/ui';
 import { TranslatePipe } from '@ngx-translate/core';
 
@@ -28,15 +28,11 @@ import { EntitiesStore } from '../../store';
 export class HistoryComponent {
   readonly #entitiesStore = inject(EntitiesStore);
 
-  activeFlag = this.#entitiesStore.activeFlag;
   entity = this.#entitiesStore.selectedEntity;
 
   #flags = computed(() => this.entity()?.flags ?? []);
   categories = computed(() => {
-    const allCategories = [
-      ...(this.activeFlag()?.categories ?? []),
-      ...(this.#flags()?.flatMap((flag) => flag.categories ?? []) ?? []),
-    ];
+    const allCategories = this.#flags()?.flatMap((flag) => flag.categories ?? []) ?? [];
     return Array.from(new Set(allCategories)).sort((a, b) => a.localeCompare(b));
   });
   rangedFlags = computed(() => this.#setRangedFlags());
@@ -46,20 +42,6 @@ export class HistoryComponent {
   containsActiveCategory(categories: FlagCategory[]): boolean {
     const activeCategories = this.activeCategories();
     return categories.some((category) => activeCategories.includes(category));
-  }
-
-  getFlag(flag: EntityFlagRange): Flag {
-    const range = this.#getRange(flag.start, flag.end);
-    return {
-      alt_parent_id: range?.alt_parent_id ?? this.entity()?.alt_parent_id,
-      end: flag.end,
-      hoistedRight: this.entity()?.hoisted_right,
-      isReversed: !!flag.reverse_url,
-      name: range?.name ?? this.entity()?.name ?? '',
-      reverse_src: flag.reverse_url ?? this.activeFlag()?.reverse_url,
-      src: flag?.url ?? this.activeFlag()?.url,
-      start: flag.start,
-    };
   }
 
   toggleCategory(category: FlagCategory) {
@@ -81,8 +63,26 @@ export class HistoryComponent {
     );
   }
 
-  #setRangedFlags(): EntityFlagRange[] {
-    const flags = this.entity()?.flags?.flatMap((flag) => flag.ranges ?? []) ?? [];
-    return flags.sort((a, b) => a.start - b.start);
+  #setFlag(flagRange: EntityFlagRange, flag?: EntityFlag): Flag {
+    const range = this.#getRange(flagRange.start, flagRange.end);
+    return {
+      alt_parent_id: range?.alt_parent_id ?? this.entity()?.alt_parent_id,
+      categories: flagRange.categories ?? flag?.categories,
+      end: flagRange.end,
+      hoistedRight: this.entity()?.hoisted_right,
+      isReversed: !!flagRange.reverse_url,
+      name: range?.name ?? this.entity()?.name ?? '',
+      reverse_src: flagRange.reverse_url ?? flag?.reverse_url,
+      src: flagRange?.url ?? flag?.url,
+      start: flagRange.start,
+    };
+  }
+
+  #setRangedFlags(): Flag[] {
+    const flags = this.entity()?.flags;
+    const rangedFlags = flags?.flatMap(
+      (flag) => flag.ranges?.map((range) => this.#setFlag(range, flag)) ?? [],
+    );
+    return (rangedFlags ?? []).sort((a, b) => (a.start ?? 0) - (b.start ?? 0));
   }
 }
